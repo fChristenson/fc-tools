fce2a9d4c0f1acdae8a58b55ae49f819c0db1e30
Fredrik <fredrik.christenson@gmail.com>
2016-12-21 10:36:39 +0100

    added label to logging

--- logging.js
+++ logging.js
@@ -1,14 +1,24 @@
 var util = require('util');
 
 // inspect : a->a
-var inspect = function(val) {
+var inspect = function(val, label) {
+  if(label) {
+    console.log(label);
+    console.log('');
+  }
+
   console.log(util.inspect(val, false, Infinity));
   console.log('----------------------');
   return val;
 };
 
 // log : a->a
-var log = function(val) {
+var log = function(val, label) {
+  if(label) {
+    console.log(label);
+    console.log('');
+  }
+
   console.log(val);
   console.log('----------------------');
   return val;
